87276c53736c51ff7c5c6b57eea01e5cc7efb0ca
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2021-03-11 11:45:07 +0100

    Recreate project vue and try implement template of login

---
+++ ponvoo/src/components/Login/Image.vue
@@ -0,0 +1,3 @@
+<template>
+  <img src="../../assets/img/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
+</template>
---
+++ ponvoo/src/views/Login.vue
@@ -0,0 +1,40 @@
+<template>
+
+  <div class="col-md-6">
+    <Image />
+  </div>
+  <div class="col-md-6 contents">
+    <FormLogin />
+  </div>
+</template>
+
+<script>
+import Image from '../components/Login/Image.vue'
+import FormLogin from '../components/Login/Form.vue'
+
+export default {
+  name: 'Login',
+  components: {
+    Image,
+    FormLogin
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+h3 {
+  margin: 40px 0 0;
+}
+ul {
+  list-style-type: none;
+  padding: 0;
+}
+li {
+  display: inline-block;
+  margin: 0 10px;
+}
+a {
+  color: #42b983;
+}
+</style>
