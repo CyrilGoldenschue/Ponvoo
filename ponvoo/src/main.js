bb9c902d73b47284e444c86e7e2210aee05a3cc8
Cyril Goldenschue <cyril.goldenschue@cpnv.ch>
2021-03-16 16:07:43 +0100

    Add import style and bootstrap

--- ponvoo/src/main.js
+++ ponvoo/src/main.js
@@ -1,5 +1,20 @@
 import { createApp } from 'vue'
 import App from './App.vue'
+
+
+import '../public/assets/css/owl.carousel.min.css'
+import '../public/assets/font/icomoon/style.css'
+
+import '../public/assets/css/bootstrap.min.css'
+
 import '../public/assets/css/style.css'
+
+
+
+
+
+
+
+
 
 createApp(App).mount('#app')
