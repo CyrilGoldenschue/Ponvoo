fbb59ff2afce608e610eda9db19dc3912036706e
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-23 16:38:41 +0100

    Move the image into the Login.Vue and delete its one-line-code component.

--- ponvoo/src/views/Login.vue
+++ ponvoo/src/views/Login.vue
@@ -4,9 +4,8 @@
     <div class="container">
       <div class="row">
         <div class="col-md-6">
-          <Image/>
+            <img src="../assets/img/undraw_remotely_2j6y.svg" alt="Image" class="img-fluid">
         </div>
-
         <FormLogin/>
       </div>
     </div>
@@ -14,14 +13,12 @@
 </template>
 
 <script>
-import Image from '../components/Login/Image.vue';
 import FormLogin from '../components/Login/Form.vue';
 import Layout from '../components/Layout.vue';
 
 export default {
   name: 'Login',
   components: {
-    Image,
     FormLogin,
     Layout
   }
