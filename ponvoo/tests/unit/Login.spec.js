df03df3e00950eecd4271ecc0fa230c22758e2a9
Mathieu <Mathieu.Burnat@cpnv.ch>
2021-03-25 11:54:19 +0100

    [In bug] Test : Get the username's value

--- ponvoo/tests/unit/Login.spec.js
+++ ponvoo/tests/unit/Login.spec.js
@@ -1,4 +1,11 @@
-// First test -> try to say hello :D
-test('sayHello', () => {
-    console.log("hello there");
+import { mount } from '@vue/test-utils'
+import Login from '@/views/Login.vue'
+
+// Try to get the input's content
+test('Get inputs content', () => {
+    const wrapper = mount(Login)
+
+    //Get the username
+    const username = wrapper.find('username').element.value
+    console.log(username)
 })
